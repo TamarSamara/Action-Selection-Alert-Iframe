fb3c748f0029557cbaa2ebd1af51af5e92627276
tamar samara <tamar.samara@gmail.com>
2022-08-15 16:25:13 +0300

    tamar

--- testautomation.py
+++ testautomation.py
@@ -1,51 +1,72 @@
+import logging
 import time
-import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-import logging
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
-url = "http://automationpractice.com/index.php"
 driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe")
-driver.get(url)
 
 
-@pytest.mark.passed
-def test_buy_dress_summer():
-    try:
+def startTest():
+    url = "http://automationpractice.com/index.php"
+    driver.get(url)
 
-        driver.find_element(By.ID, "search_query_top").send_keys("summer")
-        driver.find_element(By.NAME, "submit_search").click()
-        # msg2 = driver.find_element(By.CLASS_NAME, "lighter").text
-        # logging.warning("Error: 'Page Search 'summer'")
-        # assert msg2 == "SUMMER"
-        time.sleep(3)
+    # time.sleep(3)
 
-        driver.find_element(By.XPATH, '//*[@id="center_column"]/ul/li[1]/div/div[2]/div[2]/a[1]').click()
-        time.sleep(3)
-        # msg3 = driver.find_element(By.CLASS_NAME, "icon-ok")
-        # logging.warning("Error: 'Page: Product successfully added to your shopping cart")
-        # assert msg3 == "Product successfully added to your shopping cart"
 
-        # time.sleep(7)
-        # Click on the Close window
-        driver.find_element(By.XPATH, '//*[@id="layer_cart"]/div[1]/div[1]/span').click()
-        time.sleep(3)
-        # msg4 = driver.find_element(By.CLASS_NAME, "lighter").text
-        # logging.warning("Error: 'Page Search 'summer'")
-        # assert msg4 == "summer"
+def send_key(**kwargs):
+    if "id" in kwargs.keys():
+        driver.find_element(By.ID, kwargs["param"]).send_keys(kwargs["id"])
+    elif "xpath" in kwargs.keys():
+        driver.find_element(By.XPATH, kwargs["param"]).send_keys(kwargs["XPATH"])
+    elif "name" in kwargs.keys():
+        driver.find_element(By.NAME, kwargs["param"]).send_keys(kwargs["name"])
+    elif "class_name" in kwargs.keys():
+        driver.find_element(By.CLASS_NAME, kwargs["param"]).send_keys(kwargs["class_name"])
 
-        # Click on the Cart
-        driver.find_element(By.XPATH, '//*[@id="button_order_cart"]/span').click()
-        # msg5 = driver.find_element(By.CLASS_NAME, "page-heading").text
-        # logging.warning("Error: 'Page Shopping-cart summary'")
-        # assert msg5 == "Shopping-cart summary"
+        # time.sleep(3)
 
-        time.sleep(5)
 
-    except Exception as e:
-        print("error in code: {}".format(e))
+def click_element(**kwargs):
+    if "id" in kwargs.keys():
+        driver.find_element(By.ID, kwargs["param"]).click()
+    elif "xpath" in kwargs.keys():
+        driver.find_element(By.XPATH, kwargs["param"]).click()
+    elif "name" in kwargs.keys():
+        driver.find_element(By.NAME, kwargs["param"]).click()
+    elif "class_name" in kwargs.keys():
+        driver.find_element(By.CLASS_NAME, kwargs["param"]).click()
 
-    finally:
-        driver.quit()
+
+def test_buy_dress():
+    startTest()
+    # click button sign in
+    click_element(class_name="", param="login")
+    # search summer
+    send_key(id="summer", param="search_query_top")
+    click_element(name="", param="submit_search")
+    # click icon eye
+    click_element(xpath="", param='//*[@id="center_column"]/ul/li[3]/div/div[1]/div/div[1]/a/i')
+    time.sleep(4)
+
+    # change iframe
+    iframe = driver.find_element(By.CLASS_NAME, "fancybox-iframe")
+    driver.switch_to.frame(iframe)
+
+    # Click + to add more dresses
+    click_element(xpath="", param='//*[@id="quantity_wanted_p"]/a[2]/span/i')
+    time.sleep(3)
+    # select M from dropdown
+    sel = driver.find_element(By.ID, "group_1")
+    select = Select(sel)
+    select.select_by_value('2')
+    click_element(id="", param="color_15")
+    msg = driver.find_element(By.TAG_NAME, "h1").text
+    logging.warning("Error!")
+    assert msg == "Printed Chiffon Dress"
+
+    # click add to cart
+    click_element(xpath="", param='//*[@id="add_to_cart"]/button')
+
+    time.sleep(3)
+    driver.quit()
